b085a631bd709d8f2f0c111439ba3af61f37e5f5
Xarvex <gitlab-github.8qs1z@slmail.me>
2023-10-30 12:57:26 -0500

    Notification replacement

--- lua/plugin/noice.lua
+++ lua/plugin/noice.lua
@@ -1,41 +1,53 @@
 return {
-    "folke/noice.nvim",
-    dependencies = {
-        "MunifTanjim/nui.nvim"
-    },
-    event = "VeryLazy",
-    opts = {
-        lsp = {
-            hover = {
-                enabled = false
-            },
-            signature = {
-                enabled = false
-            },
-            override = {
-                ["cmp.entry.get_documentation"] = true,
-                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                ["vim.lsp.util.stylize_markdown"] = true
-            }
+    {
+        "folke/noice.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify"
         },
-        presets = {
-            command_palette = true,
-            long_message_to_split = true
-        },
-        views = {
-            cmdline_popup = {
-                border = {
-                    style = "none",
-                    padding = {
-                        2,
-                        3
-                    }
+        event = "VeryLazy",
+        opts = {
+            lsp = {
+                hover = {
+                    enabled = false
                 },
-                filter_options = {},
-                win_options = {
-                    winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder"
+                signature = {
+                    enabled = false
+                },
+                override = {
+                    ["cmp.entry.get_documentation"] = true,
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true
                 }
             },
+            presets = {
+                command_palette = true,
+                long_message_to_split = true
+            },
+            views = {
+                cmdline_popup = {
+                    border = {
+                        style = "none",
+                        padding = {
+                            2,
+                            3
+                        }
+                    },
+                    filter_options = {},
+                    win_options = {
+                        winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder"
+                    }
+                },
+            }
+        }
+    },
+    {
+        "rcarriga/nvim-notify",
+        lazy = true,
+        opts = {
+            render = "wrapped-compact",
+            stages = "fade"
         }
     }
+
 }
