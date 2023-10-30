99937e08537bd943b6d840c568fa9c31e555cada
Xarvex <gitlab-github.8qs1z@slmail.me>
2023-10-30 04:29:24 -0500

    Alphabetical ordering of themes

--- lua/plugin/themery.lua
+++ lua/plugin/themery.lua
@@ -20,6 +20,27 @@ return {
         }
     },
     {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        lazy = true,
+        priority = 1000,
+        opts = {
+            variant = "main",
+            dark_variant = "main",
+            disable_background = true,
+            disable_float_background = true,
+            disable_italics = true
+        }
+    },
+    {
+        "navarasu/onedark.nvim",
+        lazy = true,
+        opts = {
+            style = "darker",
+            transparent = true
+        }
+    },
+    {
         "folke/tokyonight.nvim",
         lazy = true,
         priority = 1000,
@@ -37,27 +58,6 @@ return {
             hide_inactive_statusline = false,
             dim_inactive = false,
             lualine_bold = false
-        }
-    },
-    {
-        "navarasu/onedark.nvim",
-        lazy = true,
-        opts = {
-            style = "darker",
-            transparent = true
-        }
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = true,
-        priority = 1000,
-        opts = {
-            variant = "main",
-            dark_variant = "main",
-            disable_background = true,
-            disable_float_background = true,
-            disable_italics = true
         }
     }
 }
