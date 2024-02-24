54c08bbc01dba7aeabf3f5c4a7e1a7b6ea96774f
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-02-24 14:29:34 -0600

    Allow treesitter to use language tree

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -25,6 +25,7 @@ return {
             modules = {},
             highlight = {
                 enable = true,
+                use_languagetree = true,
                 additional_vim_regex_highlighting = false
             },
             indent = { enable = true },
