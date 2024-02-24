3d4ac353e6b0068af787460504aeebbf55ebbcbb
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-02-24 17:33:04 -0600

    Add window status line

---
+++ lua/plugin/lualine.lua
@@ -0,0 +1,27 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        options = {
+            globalstatus = true,
+            component_separators = "|",
+            section_separators = { left = "", right = "" }
+        },
+        sections = {
+            lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+            lualine_b = { "filename", "branch" },
+            lualine_c = { "diagnostics" },
+            lualine_x = { "fileformat" },
+            lualine_y = { "filetype", "progress" },
+            lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } }
+        },
+        inactive_sections = {
+            lualine_a = { "filename" },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = { "location" }
+        }
+    }
+}
