a612260212dba1e97be448997853362ee33a2eba
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-03-28 17:30:46 -0500

    Modify gitsigns keymaps

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -18,27 +18,27 @@ return {
                 return "<Ignore>"
             end, { buffer = bufnr, expr = true })
 
-            vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, opts)
-            vim.keymap.set("v", "<leader>hs", function()
+            vim.keymap.set("n", "<leader>Hs", gitsigns.stage_hunk, opts)
+            vim.keymap.set("v", "<leader>Hs", function()
                 gitsigns.stage_hunk { vim.fn.line("."), vim.fn.line("v") }
             end, opts)
-            vim.keymap.set("n", "<leader>hS", gitsigns.stage_buffer, opts)
+            vim.keymap.set("n", "<leader>HS", gitsigns.stage_buffer, opts)
 
-            vim.keymap.set("n", "<leader>hu", gitsigns.undo_stage_hunk, opts)
+            vim.keymap.set("n", "<leader>Hu", gitsigns.undo_stage_hunk, opts)
 
-            vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, opts)
-            vim.keymap.set("v", "<leader>hr", function()
+            vim.keymap.set("n", "<leader>Hr", gitsigns.reset_hunk, opts)
+            vim.keymap.set("v", "<leader>Hr", function()
                 gitsigns.reset_hunk { vim.fn.line("."), vim.fn.line("v") }
             end, opts)
-            vim.keymap.set("n", "<leader>hR", gitsigns.reset_buffer, opts)
+            vim.keymap.set("n", "<leader>HR", gitsigns.reset_buffer, opts)
 
-            vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, opts)
+            vim.keymap.set("n", "<leader>Hp", gitsigns.preview_hunk, opts)
 
-            vim.keymap.set("n", "<leader>hb", function() gitsigns.blame_line { full = true } end, opts)
-            vim.keymap.set("n", "<leader>tb", gitsigns.toggle_current_line_blame, opts)
+            vim.keymap.set("n", "<leader>Hb", gitsigns.toggle_current_line_blame, opts)
+            vim.keymap.set("n", "<leader>HB", function() gitsigns.blame_line { full = true } end, opts)
 
-            vim.keymap.set("n", "<leader>hd", gitsigns.diffthis, opts)
-            vim.keymap.set("n", "<leader>hD", function() gitsigns.diffthis("~") end, opts)
+            vim.keymap.set("n", "<leader>Hd", gitsigns.diffthis, opts)
+            vim.keymap.set("n", "<leader>HD", function() gitsigns.diffthis("~") end, opts)
         end
     }
 }
