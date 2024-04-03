12791af39345e953289fcbfb784ca50b1d578535
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-04-02 22:04:10 -0500

    Reorder LSP bindings

--- lua/plugin/lsp-zero.lua
+++ lua/plugin/lsp-zero.lua
@@ -73,18 +73,22 @@ return {
 
             lsp_zero.on_attach(function(_, bufnr)
                 local opts = { buffer = bufnr, remap = false }
+
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev({ float = { border = "rounded" } }) end,
+                    opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next({ float = { border = "rounded" } }) end,
+                    opts)
+
                 vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
                 vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
                 vim.keymap.set("n", "gT", function() vim.lsp.buf.type_definition() end, opts)
                 vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
                 vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+
                 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
                 vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float({ border = "rounded" }) end, opts)
-                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev({ float = { border = "rounded" } }) end,
-                    opts)
-                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next({ float = { border = "rounded" } }) end,
-                    opts)
                 vim.keymap.set({ "n", "i" }, "<C-u>", function() vim.lsp.buf.signature_help() end, opts)
+
                 vim.keymap.set({ "n", "v" }, "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
                 vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
             end)
