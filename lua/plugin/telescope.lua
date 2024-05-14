999541f886ea19ca0b3e580bda70f2ef424526f0
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-05-13 23:03:24 -0500

    Adjust keybinds and pickers for telescope

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -11,8 +11,34 @@ return {
     },
     cmd = "Telescope",
     keys = {
-        lazy_telescope:keymap_require("n", "<leader>ff", nil, "find_files", {}),
-        lazy_telescope:keymap_require("n", "<C-p>", nil, "git_files", {}),
-        lazy_telescope:keymap_require("n", "<leader>fs", nil, "live_grep")
+        lazy_telescope:keymap_require("n", "<leader>ff", nil, "find_files"),
+        lazy_telescope:keymap_require("n", "<leader>fg", nil, "live_grep"),
+        lazy_telescope:keymap_require("n", "<leader>fb", nil, "buffers"),
+        lazy_telescope:keymap_require("n", "<leader>fh", nil, "help_tags")
+    },
+    opts = {
+        defaults = {
+            mappings = {
+                i = {
+                    ["<C-j>"] = "move_selection_next",
+                    ["<C-k>"] = "move_selection_previous"
+                }
+            }
+        },
+        pickers = {
+            buffers = { theme = "dropdown" },
+            colorscheme = {
+                -- currently not working
+                -- see https://github.com/nvim-telescope/telescope.nvim/pull/3097
+                enable_preview = true,
+                previwer = true
+            },
+            find_files = { theme = "dropdown" },
+            help_tags = {
+                theme = "dropdown",
+                previewer = false
+            },
+            live_grep = { theme = "dropdown" }
+        }
     }
 }
