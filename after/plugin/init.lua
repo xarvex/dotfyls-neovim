0d15a4bd8617cd50c8462c5fb143beb6f271abf8
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-05-14 01:17:15 -0500

    Create WezTerm colorscheme sync

--- after/plugin/init.lua
+++ after/plugin/init.lua
@@ -1,1 +1,23 @@
+local writefile = require("shortcut").writefile
 
+local wezterm = (vim.env.XDG_CONFIG_HOME or (assert(vim.env.HOME) .. "/.config")) .. "/wezterm"
+if vim.fn.isdirectory(wezterm) then
+    local wezterm_colorschemes = {
+        ["carbonfox"] = true,
+        ["duskfox"] = true,
+        ["tokyonight"] = "Tokyo Night",
+        ["oxocarbon"] = "Oxocarbon Dark (Gogh)",
+        ["rose-pine"] = true
+    }
+    vim.api.nvim_create_autocmd("ColorScheme", {
+        group = vim.api.nvim_create_augroup("wezterm_colorscheme", { clear = true }),
+        callback = function(args)
+            local colorscheme = wezterm_colorschemes[args.match]
+            if colorscheme then
+                if type(colorscheme) ~= "string" then colorscheme = args.match end
+                writefile({ colorscheme }, wezterm .. "/generated_neovim_colorscheme", "")
+                vim.notify("Setting WezTerm colorscheme to " .. colorscheme, vim.log.levels.INFO)
+            end
+        end
+    })
+end
