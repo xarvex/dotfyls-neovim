7d416937d01d4eee5a7d611d8f55c4d47d7a17f2
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-05-18 23:25:27 -0500

    Additional keybinds for Trouble

--- lua/plugin/trouble.lua
+++ lua/plugin/trouble.lua
@@ -5,7 +5,9 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     cmd = { "Trouble", "TroubleToggle" },
     keys = {
-        lazy_trouble:keymap_require("n", "<leader>xx", nil, function(trouble) trouble.toggle() end)
+        lazy_trouble:keymap_require("n", "<leader>xx", nil, function(trouble) trouble.toggle() end),
+        lazy_trouble:keymap_require("n", "<leader>xw", nil, function(trouble) trouble.toggle("workspace_diagnostics") end),
+        lazy_trouble:keymap_require("n", "<leader>xd", nil, function(trouble) trouble.toggle("document_diagnostics") end)
     },
     opts = {}
 }
