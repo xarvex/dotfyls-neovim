daf7c98f6b90dc7bdc37a2d4b0e95b8097d5f5fb
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-05-18 23:20:06 -0500

    Add all Trouble commands

--- lua/plugin/trouble.lua
+++ lua/plugin/trouble.lua
@@ -3,7 +3,7 @@ local lazy_trouble = require("lazy-load"):require("trouble")
 return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    cmd = "Trouble",
+    cmd = { "Trouble", "TroubleToggle" },
     keys = {
         lazy_trouble:keymap_require("n", "<leader>xx", nil, function(trouble) trouble.toggle() end)
     },
