ba6326758d9a6b437459d2377f75714300d6fd58
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-05-20 04:11:57 -0500

    Do not allow lsp-zero to override noice UI

--- lua/plugin/lsp-zero.lua
+++ lua/plugin/lsp-zero.lua
@@ -7,6 +7,7 @@ return {
     init = function()
         vim.g.lsp_zero_extend_cmp = 0
         vim.g.lsp_zero_extend_lspconfig = 0
+        vim.g.lsp_zero_ui_float_border = 0
 
         keymap("n", "<leader><leader>", "<Nop>")
     end,
