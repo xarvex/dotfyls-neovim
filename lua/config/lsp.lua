12f4c48845a00b496378c6b1b3b31494a0543d84
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-05-27 02:26:04 -0500

    Declare generic LSP configuration separately

---
+++ lua/config/lsp.lua
@@ -0,0 +1,31 @@
+local keymap = require("shortcut").keymap
+
+vim.api.nvim_create_autocmd("Lspattach", {
+    callback = function(args)
+        local opts = { buffer = args.buf }
+
+        keymap("n", "[d", function() vim.diagnostic.goto_prev({ float = { border = "rounded" } }) end, opts)
+        keymap("n", "]d", function() vim.diagnostic.goto_next({ float = { border = "rounded" } }) end, opts)
+
+        keymap("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        keymap("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+        keymap("n", "gT", function() vim.lsp.buf.type_definition() end, opts)
+        keymap("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+        keymap("n", "gr", function() vim.lsp.buf.references() end, opts)
+
+        keymap("n", "K", function() vim.lsp.buf.hover() end, opts)
+        keymap("n", "<leader>d", function() vim.diagnostic.open_float({ border = "rounded" }) end, opts)
+        keymap({ "n", "i" }, "<C-;>", function() vim.lsp.buf.signature_help() end, opts)
+
+        keymap({ "n", "v" }, "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+        keymap("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client ~= nil and client.server_capabilities.inlayHintProvider then
+            keymap("n", "<leader>i", function()
+                local filter = { bufnr = args.buf }
+                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(filter), filter)
+            end, opts)
+        end
+    end
+})
--- lua/plugin/lspconfig.lua
+++ lua/plugin/lspconfig.lua
@@ -12,33 +12,6 @@ return {
         local lsp_zero = require("lsp-zero")
         lsp_zero.extend_lspconfig()
 
-        lsp_zero.on_attach(function(client, bufnr)
-            local opts = { buffer = bufnr }
-
-            keymap("n", "[d", function() vim.diagnostic.goto_prev({ float = { border = "rounded" } }) end, opts)
-            keymap("n", "]d", function() vim.diagnostic.goto_next({ float = { border = "rounded" } }) end, opts)
-
-            keymap("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            keymap("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-            keymap("n", "gT", function() vim.lsp.buf.type_definition() end, opts)
-            keymap("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-            keymap("n", "gr", function() vim.lsp.buf.references() end, opts)
-
-            keymap("n", "K", function() vim.lsp.buf.hover() end, opts)
-            keymap("n", "<leader>d", function() vim.diagnostic.open_float({ border = "rounded" }) end, opts)
-            keymap({ "n", "i" }, "<C-;>", function() vim.lsp.buf.signature_help() end, opts)
-
-            keymap({ "n", "v" }, "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-            keymap("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-
-            if client.server_capabilities.inlayHintProvider then
-                keymap("n", "<leader>i", function()
-                    local filter = { bufnr = bufnr }
-                    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(filter), filter)
-                end, opts)
-            end
-        end)
-
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "rust_analyzer",
