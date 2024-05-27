2bbcde7b0d458f881e21bc432d00c4656547904a
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-05-27 18:01:52 -0500

    LuaSnip does not actually have dependencies

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -4,7 +4,7 @@ return {
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-path",
-        { "L3MON4D3/LuaSnip", dependencies = "saadparwaiz1/cmp_luasnip" },
+        "L3MON4D3/LuaSnip",
         "onsails/lspkind.nvim",
         "rafamadriz/friendly-snippets",
         "saadparwaiz1/cmp_luasnip"
