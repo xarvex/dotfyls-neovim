3863e03086c5dddb382634a22bece4c7c336e4ac
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-07-22 19:07:59 -0500

    feat(lspconfig): become mason-agnostic

--- lua/plugin/lspconfig.lua
+++ lua/plugin/lspconfig.lua
@@ -7,42 +7,86 @@ return {
     cmd = { "LspInfo", "LspInstall", "LspStart" },
     event = { "User BufNewFileFiltered", "User BufReadPostFiltered", "User BufWritePreFiltered" },
     config = function()
-        require("lsp-zero").extend_lspconfig()
-
         local keymap = require("shortcut").keymap
-        require("mason-lspconfig").setup({
-            handlers = {
-                require("lsp-zero").default_setup,
-                gopls = function()
-                    require("lspconfig").gopls.setup({
-                        settings = { gofumpt = true },
-                    })
-                end,
-                clangd = function()
-                    require("lspconfig").clangd.setup({
-                        on_attach = function(_, bufnr) keymap("n", "<leader>sh", vim.cmd.ClangdSwitchSourceHeader, { buffer = bufnr }) end,
-                    })
-                end,
-                denols = function()
-                    require("lspconfig").denols.setup({
-                        root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
-                    })
-                end,
-                tsserver = function()
-                    require("lspconfig").tsserver.setup({
-                        root_dir = require("lspconfig").util.root_pattern("tsconfig.json"),
-                    })
-                end,
-                lua_ls = function()
+
+        local servers = {
+            astro = { "astro-ls", true },
+            bashls = {
+                "bash-language-server",
+                {
+                    filetypes = { "bash", "sh", "zsh" },
+                },
+            },
+            clangd = {
+                "clangd",
+                {
+                    on_attach = function(_, bufnr) keymap("n", "<leader>sh", vim.cmd.ClangdSwitchSourceHeader, { buffer = bufnr }) end,
+                },
+            },
+            cssls = { "vscode-css-language-server", true },
+            denols = {
+                "deno",
+                {
+                    root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
+                },
+            },
+            eslint = { "vscode-eslint-language-server", true },
+            gopls = {
+                "gopls",
+                {
+                    settings = { gofumpt = true },
+                },
+            },
+            html = { "vscode-html-language-server", true },
+            jsonls = { "vscode-json-language-server", true },
+            lua_ls = {
+                "lua-language-server",
+                function()
                     require("neodev")
-                    require("lspconfig").lua_ls.setup({})
-                end,
-                bashls = function()
-                    require("lspconfig").bashls.setup({
-                        filetypes = { "bash", "sh", "zsh" },
-                    })
+
+                    return true
                 end,
             },
-        })
+            nil_ls = { "nil", true },
+            pyright = { "pyright-langserver", true },
+            rust_analyzer = { "rust-analyzer", true },
+            svelte = { "svelteserver", true },
+            tsserver = {
+                "typescript-language-server",
+                {
+                    root_dir = require("lspconfig").util.root_pattern("tsconfig.json"),
+                },
+            },
+            vuels = { "vls", true },
+        }
+
+        local function setup_server(name, skip_check)
+            local server = servers[name]
+
+            if server ~= nil and (skip_check == true or vim.fn.executable(server[1]) == 1) then
+                local setup = require("lspconfig")[name].setup
+                local config = server[2]
+
+                local config_type = type(config)
+                if config_type == "function" then
+                    local result = config()
+                    setup(result == true and {} or result)
+                else
+                    setup(config == true and {} or config)
+                end
+            end
+        end
+
+        require("lsp-zero").extend_lspconfig()
+
+        if require("env").mason_used then
+            require("mason-lspconfig").setup({
+                handlers = { function(name) setup_server(name, true) end },
+            })
+        else
+            for name, _ in pairs(servers) do
+                setup_server(name)
+            end
+        end
     end,
 }
