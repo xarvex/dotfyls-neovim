8dac63cadb4b3b2f3f17a3fa1376770e7407f883
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-31 22:21:10 -0500

    fix(themery): move from deprecated config

--- lua/generate.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-local generated = vim.fs.joinpath(vim.fn.stdpath("config"), "lua/generated")
-
-M.path = function(fname) return vim.fs.joinpath(generated, fname) end
-
-return M
--- lua/plugin/themery.lua
+++ lua/plugin/themery.lua
@@ -1,20 +1,9 @@
-local themery = require("generate").path("themery.lua")
-
 return {
     "zaldih/themery.nvim",
     cmd = "Themery",
     keys = {
         { "<C-A-t>", vim.cmd.Themery },
     },
-    init = function()
-        if not require("shortcut").filereadable(themery) then
-            require("shortcut").writefile({
-                "-- Themery block",
-                "-- end themery block",
-            }, themery, "")
-        end
-        pcall(require, "generated.themery") -- not guaranteed to exist
-    end,
     opts = {
         -- stylua: ignore
         themes = {
@@ -23,6 +12,5 @@ return {
             "rose-pine",
             "tokyonight",
         },
-        themeConfigFile = themery,
     },
 }
