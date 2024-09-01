486855efe93d8935ff0d2f1500363f2e4665266f
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-31 22:00:04 -0500

    nit(env): format and lint

--- lua/env.lua
+++ lua/env.lua
@@ -1,6 +1,5 @@
 local M = {}
 
-M.mason_used = vim.env.DOTFYLS_NEOVIM_MASON ~= nil and vim.env.DOTFYLS_NEOVIM_MASON == "1"
-    or vim.fn.executable("nix") == 0
+M.mason_used = vim.env.DOTFYLS_NEOVIM_MASON ~= nil and vim.env.DOTFYLS_NEOVIM_MASON == "1" or vim.fn.executable("nix") == 0
 
 return M
