1df36dcfe8a28c2dfb408f8fb0c71a6bc2a0ce60
xarvex <gitlab-github.8qs1z@slmail.me>
2024-09-01 14:29:03 -0500

    nix(dressing): ordering

--- lua/plugin/dressing.lua
+++ lua/plugin/dressing.lua
@@ -4,13 +4,13 @@ return {
     init = function()
         local lazy_dressing = require("lazy-load"):require("dressing")
 
-        vim.ui.select = function(...)
-            lazy_dressing:load()
-            vim.ui.select(...)
-        end
         vim.ui.input = function(...)
             lazy_dressing:load()
             vim.ui.input(...)
+        end
+        vim.ui.select = function(...)
+            lazy_dressing:load()
+            vim.ui.select(...)
         end
     end,
 }
