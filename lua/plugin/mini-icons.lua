613401a2a99756c3585170aa937c574cda4609ae
xarvex <gitlab-github.8qs1z@slmail.me>
2024-09-01 16:31:53 -0500

    fix(mini-icons): remove old workaround

--- lua/plugin/mini-icons.lua
+++ lua/plugin/mini-icons.lua
@@ -3,13 +3,7 @@ return {
     specs = { { "nvim-tree/nvim-web-devicons", enabled = false, optional = true } },
     lazy = true,
     init = function()
-        package.preload["nvim-web-devicons"] = function()
-            -- Needed as it will be false when loading and cause failure.
-            -- See: https://github.com/echasnovski/mini.nvim/issues/1007#issuecomment-2206571684
-            package.loaded["nvim-web-devicons"] = {}
-            require("mini.icons").mock_nvim_web_devicons()
-            return package.loaded["nvim-web-devicons"]
-        end
+        package.preload["nvim-web-devicons"] = function() require("mini.icons").mock_nvim_web_devicons() end
     end,
     opts = {},
 }
