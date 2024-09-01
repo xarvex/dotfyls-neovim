5263bb02d183e48619f7197cbca9193cbe6872b0
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-31 21:25:28 -0500

    chore(flake): adjust with linting and formatting

--- flake.nix
+++ flake.nix
@@ -9,60 +9,77 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = { flake-parts, nixpkgs, self, systems, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = import systems;
+  outputs =
+    {
+      flake-parts,
+      nixpkgs,
+      self,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-    perSystem = { system, ... }:
-      let
-        pkgs = import nixpkgs { inherit system; };
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs { inherit system; };
 
-        # this currently does not work, unable to import modules
-        neovim-wrapped = pkgs.symlinkJoin {
-          name = pkgs.neovim-unwrapped.name;
-          paths = [ pkgs.neovim-unwrapped ];
-          buildInputs = with pkgs; [ makeWrapper ];
-          postBuild = ''
-            wrapProgram "$out"/bin/nvim --add-flags '-u '${self}'/init.lua'
-          '';
-        };
-      in
-      {
-        packages = rec {
-          default = neovim;
+          # This currently does not work, unable to import modules.
+          neovim-wrapped = pkgs.symlinkJoin {
+            inherit (pkgs.neovim-unwrapped) name;
 
-          neovim = pkgs.neovim-unwrapped; # will be bundled with config
-        };
-        apps = rec {
-          default = neovim;
+            paths = [ pkgs.neovim-unwrapped ];
+            nativeBuildInputs = with pkgs; [ makeWrapper ];
+            postBuild = ''
+              wrapProgram "''${out}"/bin/nvim --add-flags '-u '${self}'/init.lua'
+            '';
+          };
+        in
+        {
+          packages = rec {
+            default = neovim;
 
-          neovim = {
-            type = "app";
-            program = "${neovim-wrapped}/bin/nvim";
+            # Will be bundled with config.
+            neovim = pkgs.neovim-unwrapped;
+          };
+          apps = rec {
+            default = neovim;
+
+            neovim = {
+              type = "app";
+              program = "${neovim-wrapped}/bin/nvim";
+            };
           };
         };
+
+      flake.homeManagerModules = rec {
+        default = neovim;
+
+        neovim =
+          {
+            config,
+            lib,
+            pkgs,
+            ...
+          }:
+          lib.mkIf config.programs.neovim.enable {
+            programs.neovim = {
+              withNodeJs = lib.mkDefault false;
+              withPython3 = lib.mkDefault false;
+              withRuby = lib.mkDefault false;
+              extraPackages = with pkgs; [
+                clang
+                git
+                gnumake
+                ripgrep
+              ];
+            };
+
+            xdg.configFile.nvim = {
+              recursive = true;
+              source = ./.;
+            };
+          };
       };
-
-    flake.homeManagerModules = rec {
-      default = neovim;
-
-      neovim = ({ config, lib, pkgs, ... }: lib.mkIf config.programs.neovim.enable {
-        programs.neovim = {
-          withNodeJs = lib.mkDefault false;
-          withPython3 = lib.mkDefault false;
-          withRuby = lib.mkDefault false;
-          extraPackages = with pkgs; [
-            clang
-            git
-            gnumake
-            ripgrep
-          ];
-        };
-
-        xdg.configFile.nvim = {
-          recursive = true;
-          source = ./.;
-        };
-      });
     };
-  };
 }
