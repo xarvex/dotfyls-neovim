642000d1e896af293ed71cde88c967f381b0d536
xarvex <gitlab-github.8qs1z@slmail.me>
2024-09-01 17:56:54 -0500

    nix(flake): just use PWD

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       systems = import inputs.systems;
 
       perSystem =
-        { config, pkgs, ... }:
+        { pkgs, ... }:
         let
           inherit (nixpkgs) lib;
 
@@ -66,51 +66,47 @@
           devenv.shells = rec {
             default = neovim;
 
-            neovim =
-              let
-                cfg = config.devenv.shells.neovim;
-              in
-              {
-                devenv.root =
-                  let
-                    devenvRoot = builtins.readFile inputs.devenv-root.outPath;
-                  in
-                  # If not overriden (/dev/null), --impure is necessary.
-                  lib.mkIf (devenvRoot != "") devenvRoot;
+            neovim = {
+              devenv.root =
+                let
+                  devenvRoot = builtins.readFile inputs.devenv-root.outPath;
+                in
+                # If not overriden (/dev/null), --impure is necessary.
+                lib.mkIf (devenvRoot != "") devenvRoot;
 
-                name = "Neovim";
+              name = "Neovim";
 
-                packages = [
-                  pkgs.neovim
-                  pkgs.nodejs_22
-                ];
+              packages = [
+                pkgs.neovim
+                pkgs.nodejs_22
+              ];
 
-                enterShell = ''
-                  NVIM_APPNAME="dotfyls/devshell/nvim/$(sha1sum - <<<"${cfg.devenv.root}" | head -c40)''${DEVENV_ROOT//[^a-zA-Z0-9]/-}"
-                  export NVIM_APPNAME
-                  mkdir -p "''${XDG_CONFIG_HOME:-''${HOME}/.config}/dotfyls/devshell/nvim"
-                  ln -fsT "${cfg.devenv.root}" "''${XDG_CONFIG_HOME:-''${HOME}/.config}/''${NVIM_APPNAME}"
-                '';
+              enterShell = ''
+                NVIM_APPNAME="dotfyls/devshell/nvim/$(sha1sum - <<<"''${PWD}" | head -c40)''${PWD//[^a-zA-Z0-9]/-}"
+                export NVIM_APPNAME
+                mkdir -p "''${XDG_CONFIG_HOME:-''${HOME}/.config}/dotfyls/devshell/nvim"
+                ln -fsT "''${PWD}" "''${XDG_CONFIG_HOME:-''${HOME}/.config}/''${NVIM_APPNAME}"
+              '';
 
-                languages = {
-                  lua = {
-                    enable = true;
-                    package = pkgs.luajit;
-                  };
-                  nix.enable = true;
+              languages = {
+                lua = {
+                  enable = true;
+                  package = pkgs.luajit;
                 };
-
-                pre-commit.hooks = {
-                  deadnix.enable = true;
-                  flake-checker.enable = true;
-                  nixfmt = {
-                    enable = true;
-                    package = pkgs.nixfmt-rfc-style;
-                  };
-                  statix.enable = true;
-                  stylua.enable = true;
-                };
+                nix.enable = true;
               };
+
+              pre-commit.hooks = {
+                deadnix.enable = true;
+                flake-checker.enable = true;
+                nixfmt = {
+                  enable = true;
+                  package = pkgs.nixfmt-rfc-style;
+                };
+                statix.enable = true;
+                stylua.enable = true;
+              };
+            };
           };
         };
 
