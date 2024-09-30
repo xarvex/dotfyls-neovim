4bec23dc7a30f50ad34e2453d387e1de240c2141
xarvex <gitlab-github.8qs1z@slmail.me>
2024-09-30 18:46:36 -0500

    fix(LuaSnip): optionally use make

--- lua/dotfyls/plugins/LuaSnip.lua
+++ lua/dotfyls/plugins/LuaSnip.lua
@@ -1,7 +1,8 @@
 return {
     {
+        -- DEPENDENCIES: gnumake (optional) for building jsregexp.
         "L3MON4D3/LuaSnip",
-        build = "make install_jsregexp",
+        build = vim.fn.executable("make") == 1 and "make install_jsregexp" or nil,
         dependencies = {
             {
                 "rafamadriz/friendly-snippets",
